fa846ba194b6469978f5b0571693d0d882feeae1
wangliu0416 <liuwang0416@gmail.com>
2019-04-28 16:15:32 +0800

    2019年4月28日16:14:35 @liuwang 添加功能：解压文件（参数1：输出目录，参数2：输入解压文件）

--- file_functions.py
+++ file_functions.py
@@ -1,5 +1,6 @@
 # coding:utf-8
 import os
+import zipfile
 
 
 # 删除文件夹里面的文件，如果存在子文件夹，同时删除子文件夹中的文件（不会删除文件夹）
@@ -11,6 +12,19 @@ def del_filesByPath(path):
             del_filesByPath(c_path)
         else:
             os.remove(c_path)
+
+
+# 解压文件（参数1：输出目录，参数2：输入要解压的文件）
+def unpack_file(output_path, file_name):
+    """unzip zip file"""
+    zip_file = zipfile.ZipFile(file_name)
+    if os.path.isdir(output_path):
+        pass
+    else:
+        os.mkdir(output_path)
+    for names in zip_file.namelist():
+        zip_file.extract(names, output_path)
+    zip_file.close()
 
 
 # if __name__ == '__main__':
